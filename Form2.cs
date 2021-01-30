5f43d55ddaf3df5901d5eb5eccba7e6c624a57ea
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2021-01-30 16:31:26 +0000

    Now supports canary
    Added support for canary builds alongside master :)
    Also added info screen for links to Xenia.jp, their github and this github

---
+++ Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+
+namespace XeniaUpdater
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://xenia.jp");
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           Process.Start("https://github.com/xenia-project");
+           this.Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/Chopper1337/XeniaUpdater");
+            this.Close();
+        }
+    }
+}
