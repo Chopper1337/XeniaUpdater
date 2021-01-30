23b09528f4ef741a73c012d39165193708e1d3fa
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2021-01-30 15:07:23 +0000

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace XeniaUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(7, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(235, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 34);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Xenia Updater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+
+namespace XeniaUpdater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Some default form thing idk what this does but it's added automatically :))
+            InitializeComponent();
+
+            //The URL to download the latest release of Xenia
+            const string zipUrl = "https://ci.appveyor.com/api/projects/benvanik/xenia/artifacts/xenia_master.zip?branch=master&job=Configuration%3A%20Release&pr=false";
+
+            //Using a web client, dow the following
+            using (WebClient webClient = new WebClient())
+            {
+                //Delete the old zip
+                File.Delete("xenia_master_old.zip");
+
+                //Try rename current version to old and delete current version
+                try
+                {
+                    File.Move("xenia_master.zip", "xenia_master_old.zip");
+                    File.Delete("xenia.pdb");
+                    File.Delete("LICENSE");
+                    File.Delete("xenia.log");
+                    File.Delete("xenia.exe");
+                    File.Delete("xenia_old.exe");
+                }
+                catch (Exception)
+                {
+                }
+
+                //Download the file xenia_master.zip from the url supplied in the string zipUrl
+                webClient.DownloadFileAsync(new Uri(zipUrl), "xenia_master.zip");
+                
+                //For each change in progrress, output progress to the wc_DownloadProgressChanged method
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(wc_DownloadProgressChanged);
+
+                //Clear the web client
+                webClient.Dispose();
+            }
+
+            // For each update in the downloads progress, do this
+            void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+            {
+                //Value of the progress bar is equal to the percentage of progress of the download
+                progressBar1.Value = e.ProgressPercentage;
+
+                //The percentage string is equal to the value of the progress bar
+                string percentage = Convert.ToString(progressBar1.Value);
+
+                //Show the percentage
+                label1.Text = $"{percentage}%";
+
+                //Integer to represent the downloads progress
+                //This isn't visible to the user
+                int percentInt = int.Parse(percentage);
+
+                //If the download progress is 100%, do the finalize method
+                if (percentInt == 100)
+                {
+                    finalize();
+
+                    //Add one to the int to prevent the finalize method being called over and over
+                    percentInt++;
+                }
+            }
+
+            //Finalize
+            void finalize()
+            {
+
+                //For each process with the name "xenia"
+                foreach (var process in Process.GetProcessesByName("xenia"))
+                {
+                    //Kill process
+                    process.Kill();
+                }
+
+                //Extracts the zip to the current directory
+                //In try/catch to surpress potential errors
+                try
+                {
+                    ZipFile.ExtractToDirectory("xenia_master.zip", ".");
+                }
+                catch (Exception)
+                {
+                }
+
+                //Once all is done, start Xenia.
+                Process.Start("xenia.exe");
+
+                //Exit
+                this.Close();
+            }
+        }
+    }
+}
