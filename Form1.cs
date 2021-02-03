37dd671873633461fbce9af95be2cc95523b90c6
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2021-02-03 06:38:17 +0000

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,7 @@ namespace XeniaUpdater
     public partial class Form1 : Form
     {
         //Important strings
+        //updaterBranch on Form2.cs is also important :)
         const string masterURL = "https://ci.appveyor.com/api/projects/benvanik/xenia/artifacts/xenia_master.zip?branch=master&job=Configuration%3A%20Release&pr=false";
         const string canaryURL = "https://ci.appveyor.com/api/projects/chris-hawley/xenia-canary/artifacts/xenia_canary.zip?branch=canary_new&job=Configuration:%20Release&pr=false";
         const string masterEXE = "xenia.exe";
