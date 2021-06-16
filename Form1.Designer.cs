cfbf8b0eee87274d4b826ad3e04d037e02ba31ae
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2021-06-16 01:23:37 +0100

    Added option to upload log
    Added new button to upload the xenia.log file to temp.ninja.
    This requires having CURL installed.
    Right now the button is just quickly added in for the sake of having it there.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,6 +44,7 @@ namespace XeniaUpdater_C
             this.infoBTN = new System.Windows.Forms.Button();
             this.updateBTN = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // masterUpdateBTN
@@ -183,11 +184,22 @@ namespace XeniaUpdater_C
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_2);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Upload Log";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(272, 155);
+            this.ClientSize = new System.Drawing.Size(272, 180);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.updateBTN);
             this.Controls.Add(this.infoBTN);
@@ -228,6 +240,7 @@ namespace XeniaUpdater_C
         private System.Windows.Forms.Button infoBTN;
         private System.Windows.Forms.Button updateBTN;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
