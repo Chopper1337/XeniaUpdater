2baacbb8a08494e3f4e8e32d8419d1c542bd75f1
Chopper1337 <josephpspbikes@hotmail.co.uk>
2021-08-09 00:22:21 +0100

    Making sure Debug is at same point as Release

--- Form1.cs
+++ Form1.cs
@@ -135,7 +135,7 @@ namespace XeniaUpdater_C
         private void updateBTN_Click(object sender, EventArgs e)
         {
             Helper h = new Helper();
-            h.UpdateXeniaUpdater("Release");
+            h.UpdateXeniaUpdater("Debug");
         }
 
         private void button1_Click_2(object sender, EventArgs e)
