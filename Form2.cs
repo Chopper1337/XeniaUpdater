2fa1c28f8c3faabc20ac8734dc694a9d185726c1
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2021-10-08 21:43:53 +0100

    Fixed log error. Update Form2.cs, Helper.cs, and 6 more files...

--- Form2.cs
+++ Form2.cs
@@ -8,7 +8,7 @@ namespace XeniaUpdater_C
     {
         //KEEP \N AT THE END. GITHUB RETURNS TEXT FILES WITH THIS FOR SOME REASON
         //UPDATE CHECK WILL NOT WORK IF REMOVED.
-        public string buildDate = "Build date: 08/10/2021 2100 GMT\n";
+        public string buildDate = "Build date: 08/10/2021 2110 GMT\n";
         public Form2()
         {
             InitializeComponent();
