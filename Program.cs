c6fc458a824c156455f440b2414a21f2d8e6d71f
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2021-10-08 21:30:46 +0100

    Very minor changes. Update Form1.cs, Form2.cs, and 13 more files...

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace XeniaUpdater_C
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+           Application.EnableVisualStyles();
+           Application.SetCompatibleTextRenderingDefault(false);
+           Application.Run(new Form1());
+       }
     }
 }
